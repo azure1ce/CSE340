fe73fa13c125d80c4e125ee2e874da551bb41507
azure1ce <tanjingming@msn.cn>
2020-02-04 04:14:37 -0700

    project 3

---
+++ proj3/inputbuf.h
@@ -0,0 +1,24 @@
+/*
+JINGMING TAN
+1209438669
+ARIZONA STATE UNIVERSITY
+CSE340
+ */
+#ifndef __INPUT_BUFFER__H__
+#define __INPUT_BUFFER__H__
+
+#include <string>
+#include <vector>
+
+class InputBuffer {
+  public:
+    void GetChar(char&);
+    char UngetChar(char);
+    std::string UngetString(std::string);
+    bool EndOfInput();
+
+  private:
+    std::vector<char> input_buffer;
+};
+
+#endif  //__INPUT_BUFFER__H__
